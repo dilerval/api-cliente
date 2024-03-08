0cf7d341ccb89d1bc97812eecaa9fe4498a097d2
Dilerval Carvalho Silva <dilerval@dilerval.com.br>
2024-03-08 09:06:42 -0300

    01 - 05 - Valor único

--- clientes/models.py
+++ clientes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30, )
+    # Era pra inserir unique=True para gerar o validator unico para cpf, mas já estava presente
     cpf = models.CharField(max_length=11, unique=True)
     rg = models.CharField(max_length=9)
     celular = models.CharField(max_length=14)
