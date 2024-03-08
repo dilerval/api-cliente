23fc5d43b36bebb56c10eb8d0d0e555ffef94d4e
Dilerval Carvalho Silva <dilerval@dilerval.com.br>
2024-03-08 09:23:47 -0300

    02-03-Validando todos os campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -9,4 +9,16 @@ class ClienteSerializer(serializers.ModelSerializer):
         if len(cpf) != 11:
             raise serializers.ValidationError('O CPF deve ter 11 dígitos.')
         return cpf
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('Não inclua números nesse campo.')
+        return nome
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RGCPF deve ter 9 dígitos.')
+        return rg
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError('O celular deve ter 11 dígitos.')
+        return celular
 
